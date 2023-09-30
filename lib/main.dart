7d699a45bc494b3dca3265273092793f270141e2
erazinkov <egorrazinkov@gmail.com>
2023-09-30 23:50:30 +0300

    added user

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+// import 'package:transparent_image/transparent_image.dart';
 
 Random random = Random();
 
@@ -21,6 +23,98 @@ Future<Album> fetchAlbum() async {
     // If the server did not return a 200 OK response,
     // then throw an exception.
     throw Exception('Failed to load album');
+  }
+}
+
+Future<Photo> fetchPhoto() async {
+  int randomId = random.nextInt(9) + 1;
+
+  final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/photos/${randomId}'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Photo.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+Future<User> fetchUser() async {
+  final response =
+      await http.get(Uri.parse('http://localhost:3000/api/users/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Photo {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  const Photo({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      albumId: json['albumId'],
+      id: json['id'],
+      title: json['title'],
+      url: json['url'],
+      thumbnailUrl: json['thumbnailUrl'],
+    );
+  }
+}
+
+class User {
+  final int id;
+  final String email;
+  final String role;
+  final String firstName;
+  final String lastName;
+  final String dateOfBirth;
+  final String photo;
+  final String nickName;
+
+  const User({
+    required this.id,
+    required this.email,
+    required this.role,
+    required this.firstName,
+    required this.lastName,
+    required this.dateOfBirth,
+    required this.photo,
+    required this.nickName,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      email: json['email'],
+      role: json['role'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      dateOfBirth: json['dateOfBirth'],
+      photo: json['photo'],
+      nickName: json['nickName'],
+    );
   }
 }
 
@@ -54,17 +148,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<Album> futureAlbum;
+  late Future<User> futureUser;
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    futureUser = fetchUser();
   }
 
   void onRefreshAlbum() {
     setState(() {
-      futureAlbum = fetchAlbum();
+      futureUser = fetchUser();
     });
   }
 
@@ -83,11 +177,33 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              FutureBuilder<Album>(
-                future: futureAlbum,
+              FutureBuilder<User>(
+                future: futureUser,
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    return Text(snapshot.data!.title);
+                    return Column(children: [
+                      Text((snapshot.data!.id).toString()),
+                      // Image.network(
+                      //   snapshot.data!.thumbnailUrl,
+                      //   // fit: BoxFit.fill,
+                      //   loadingBuilder: (BuildContext context, Widget child,
+                      //       ImageChunkEvent? loadingProgress) {
+                      //     if (loadingProgress == null) return child;
+                      //     return Center(
+                      //       child: CircularProgressIndicator(
+                      //         value: loadingProgress.expectedTotalBytes != null
+                      //             ? loadingProgress.cumulativeBytesLoaded /
+                      //                 loadingProgress.expectedTotalBytes!
+                      //             : null,
+                      //       ),
+                      //     );
+                      //   },
+                      // )
+                      // FadeInImage(
+                      //   placeholder: MemoryImage(kTransparentImage),
+                      //   image: NetworkImage(snapshot.data!.thumbnailUrl),
+                      // ),
+                    ]);
                   } else if (snapshot.hasError) {
                     return Text('${snapshot.error}');
                   }
