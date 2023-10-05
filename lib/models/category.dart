ef14fad68a5ce2965ea593c2baf05f4eb4173668
erazinkov <egorrazinkov@gmail.com>
2023-10-05 15:12:55 +0300

    s11p203

---
+++ lib/models/category.dart
@@ -0,0 +1,17 @@
+enum CategoryType {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other,
+}
+
+class Category {
+  Category({required this.type});
+  final CategoryType type;
+}
