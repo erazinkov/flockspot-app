920249710d186f002bcbb297f49e332e6e585fe7
erazinkov <egorrazinkov@gmail.com>
2023-10-16 18:53:56 +0300

    small fix

--- lib/screens/flocks_dummy.dart
+++ lib/screens/flocks_dummy.dart
@@ -4,6 +4,8 @@ import 'package:first_app/models/vibe.dart';
 import 'package:first_app/screens/flock.dart';
 import 'package:first_app/widgets/vibe_item.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class FlocksScreen extends StatefulWidget {
   const FlocksScreen({super.key});
@@ -15,6 +17,20 @@ class FlocksScreen extends StatefulWidget {
 class _FlocksScreenState extends State<FlocksScreen> {
   final List<Flock> _flockItems = dummyFlocks;
   final List<Vibe> _vibeItems = dummyVibes;
+
+  final url =
+      Uri.parse('http://10.0.2.2:3000/api/flocks/paging?take=10&skip=0');
+  void getData() async {
+    final response = await http.get(url);
+    var data = json.decode(response.body);
+    print(data);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({
