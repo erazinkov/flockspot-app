a02b8a2b68a2db6ef59a0de842542df105f671b3
erazinkov <egorrazinkov@gmail.com>
2023-11-03 17:48:08 +0300

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:first_app/models.dart';
+import 'package:first_app/requests.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -87,39 +89,59 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          child: StreamBuilder(
+        stream: getNumbers(Duration(seconds: 1)),
+        initialData: Number(0),
+        builder: (context, stream) {
+          if (stream.connectionState == ConnectionState.done) {
+            return Icon(
+              Icons.check_circle,
+              color: Colors.green,
+              size: 20,
+            );
+          }
+          if (stream.hasData) {
+            return LikeCounter(stream.data!.value);
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      )),
+    );
+  }
+}
+
+class LikeCounter extends StatelessWidget {
+  static List<Color> colors = [
+    Colors.green,
+    Colors.purpleAccent,
+    Colors.deepPurple,
+    Colors.blueAccent,
+    Colors.deepOrangeAccent
+  ];
+  final int num;
+
+  LikeCounter(this.num);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 250),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: colors[num % colors.length],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(Icons.favorite, color: Colors.white),
+          Text(
+            " $num Likes",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ],
+      ),
     );
   }
 }
